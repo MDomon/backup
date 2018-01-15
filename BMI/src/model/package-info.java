ffadf2ad7a32c774fd50c7b9f0c6102ff1ec4206
MDomon <masamichi.d.engineer@gmail.com>
2018-01-15 17:46:24 +0900

    1/15

---
+++ BMI/src/model/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *
+ */
+/**
+ * @author internousdev
+ *
+ */
+package model;
+
---
+++ Register/src/model/RegisterUserLogic.java
@@ -0,0 +1,9 @@
+package model;
+
+public class RegisterUserLogic {
+	public boolean register(User user){
+		//登録処理（サンプルでは行わない
+		return true;
+	}
+
+}
